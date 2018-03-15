64f0f74474bb0450a28aec620c7d69b2994da4e4
Hugo FAZIO <hugo.fazio@igtp3-mac10.isim.intra>
2018-03-15 15:09:17 +0100

    test

---
+++ P(z)KTests/P_z_KTests.swift
@@ -0,0 +1,36 @@
+//
+//  P_z_KTests.swift
+//  P(z)KTests
+//
+//  Created by Thais AURARD on 28/02/2018.
+//  Copyright © 2018 Thais AURARD. All rights reserved.
+//
+
+import XCTest
+@testable import P_z_K
+
+class P_z_KTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
